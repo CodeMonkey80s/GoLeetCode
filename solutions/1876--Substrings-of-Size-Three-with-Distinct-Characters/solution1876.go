dbee2b2bc453a101736766971a70b805101786fe
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-10-10 19:29:15 +0200

    update 1876 housekeeping

--- solutions/1876--Substrings-of-Size-Three-with-Distinct-Characters/solution1876.go
+++ solutions/1876--Substrings-of-Size-Three-with-Distinct-Characters/solution1876.go
@@ -12,28 +12,18 @@ package solution1876
 	goarch: amd64
 	pkg: GoLeetCode/solutions/1876--Substrings-of-Size-Three-with-Distinct-Characters
 	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
-	Benchmark_countGoodSubstrings-24    	213954763	         5.636 ns/op	       0 B/op	       0 allocs/op
+	Benchmark_countGoodSubstrings-24    	788855241	         1.516 ns/op	       0 B/op	       0 allocs/op
 	PASS
 
 */
 
 func countGoodSubstrings(s string) int {
 	ans := 0
-	var freq uint32
 	for i := 0; i < len(s)-2; i++ {
-		freq = 0
-		a := s[i] - 'a'
-		b := s[i+1] - 'a'
-		c := s[i+2] - 'a'
-		if freq&(1<<a) > 0 {
-			continue
-		}
-		freq |= 1 << a
-		if freq&(1<<b) > 0 {
-			continue
-		}
-		freq |= 1 << b
-		if freq&(1<<c) > 0 {
+		a := s[i]
+		b := s[i+1]
+		c := s[i+2]
+		if a == b || b == c || c == a {
 			continue
 		}
 		ans++
