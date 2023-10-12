1e859bfcf3d605d6319c3b1023505fad471927b0
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-10-12 18:35:11 +0200

    update 2185 housekeeping

--- solutions/2185---Counting-Words-With-a-Given-Prefix/solution2185.go
+++ solutions/2185---Counting-Words-With-a-Given-Prefix/solution2185.go
@@ -14,7 +14,8 @@ import "strings"
 	goarch: amd64
 	pkg: GoLeetCode/solutions/2185---Counting-Words-With-a-Given-Prefix
 	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
-	Benchmark_prefixCount-24    	144226126	         8.646 ns/op	       0 B/op	       0 allocs/op
+	Benchmark_prefixCount-24                  	143253672	         9.199 ns/op	       0 B/op	       0 allocs/op
+	Benchmark_prefixCount_strings_index-24    	165435376	         7.407 ns/op	       0 B/op	       0 allocs/op
 	PASS
 
 */
@@ -35,7 +36,7 @@ func prefixCount(words []string, pref string) int {
 func prefixCount_strings_index(words []string, pref string) int {
 	ans := 0
 	for _, word := range words {
-		if strings.Index(word, pref) != -1 {
+		if strings.HasPrefix(word, pref) {
 			ans++
 		}
 	}
