3194fa95d3ee3576473e116a702a338eba0d1480
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-10-14 23:04:29 +0200

    update 1 housekeeping

--- solutions/1---Two-Sum/solution1.go
+++ solutions/1---Two-Sum/solution1.go
@@ -5,16 +5,29 @@ package solution1
 // URL: https://leetcode.com/problems/two-sum/
 // ============================================================================
 
+/*
+
+	$ go test -bench=. -benchmem
+	goos: linux
+	goarch: amd64
+	pkg: GoLeetCode/solutions/1---Two-Sum
+	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
+	Benchmark_twoSum_Iterations-24    	100000000	        13.31 ns/op	      16 B/op	       1 allocs/op
+	Benchmark_twoSum_Map-24           	38308172	        29.08 ns/op	      16 B/op	       1 allocs/op
+	PASS
+
+*/
+
 func twoSum_Iterations(nums []int, target int) []int {
-	max := len(nums)
-	if max < 2 || max > 10_000 {
+	maxval := len(nums)
+	if maxval < 2 || maxval > 10_000 {
 		return []int{}
 	}
-	if max == 2 && nums[0]+nums[1] == target {
+	if maxval == 2 && nums[0]+nums[1] == target {
 		return []int{0, 1}
 	}
-	for i := 0; i < max; i++ {
-		for j := i + 1; j < max; j++ {
+	for i := 0; i < maxval; i++ {
+		for j := i + 1; j < maxval; j++ {
 			t := nums[i] + nums[j]
 			if t == target {
 				return []int{i, j}
@@ -27,15 +40,15 @@ func twoSum_Iterations(nums []int, target int) []int {
 // ============================================================================
 
 func twoSum_Map(nums []int, target int) []int {
-	max := len(nums)
-	if max < 2 || max > 10_000 {
+	maxval := len(nums)
+	if maxval < 2 || maxval > 10_000 {
 		return []int{}
 	}
-	if max == 2 && nums[0]+nums[1] == target {
+	if maxval == 2 && nums[0]+nums[1] == target {
 		return []int{0, 1}
 	}
 	var t int
-	m := make(map[int]int, max)
+	m := make(map[int]int, maxval)
 	for k, v := range nums {
 		t = target - v
 		_, ok := m[t]
