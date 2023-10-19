e0fa48fcc8552fe2b4edd06914191a3350d676a9
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-10-19 20:46:26 +0200

    add tests for 344 housekeeping

--- solutions/344---Reverse-String/solution344.go
+++ solutions/344---Reverse-String/solution344.go
@@ -5,6 +5,18 @@ package solution344
 // URL: https://leetcode.com/problems/reverse-string/
 // ============================================================================
 
+/*
+
+	$ go test -bench=. -benchmem
+	goos: linux
+	goarch: amd64
+	pkg: GoLeetCode/solutions/344---Reverse-String
+	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
+	Benchmark_reverse-24    	1000000000	         1.157 ns/op	       0 B/op	       0 allocs/op
+	PASS
+
+*/
+
 func reverseString(s []byte) {
 	size := len(s)
 	for i := 0; i < size/2; i++ {
