8befa0afc4f068b4d0c680b2d69cc7baad217bf6
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2024-09-29 19:55:29 +0200

    upd 1051

--- solutions/1051---Height-Checker/solution1051.go
+++ solutions/1051---Height-Checker/solution1051.go
@@ -1,7 +1,7 @@
 package solution1051
 
 import (
-	"sort"
+	"slices"
 )
 
 // ============================================================================
@@ -16,7 +16,7 @@ import (
 	goarch: amd64
 	pkg: GoLeetCode/solutions/1051---Height-Checker
 	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
-	Benchmark_heightChecker-24    	17535759	        63.04 ns/op	      72 B/op	       2 allocs/op
+	Benchmark_heightChecker-24    	38655662	        30.87 ns/op	      48 B/op	       1 allocs/op
 	PASS
 
 */
@@ -25,7 +25,7 @@ func heightChecker(heights []int) int {
 	ans := 0
 	sorted := make([]int, len(heights))
 	copy(sorted, heights)
-	sort.Ints(sorted)
+	slices.Sort(sorted)
 	for i, v := range sorted {
 		if v != heights[i] {
 			ans++
