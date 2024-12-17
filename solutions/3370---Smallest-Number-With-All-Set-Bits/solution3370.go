c3421b5b7c9d7f01d9b09a758e7c5ac9817f863b
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2024-12-17 18:04:12 +0100

    add 3340 add 3370

---
+++ solutions/3370---Smallest-Number-With-All-Set-Bits/solution3370.go
@@ -0,0 +1,31 @@
+package solution3370
+
+import "math"
+
+// ============================================================================
+// 3370. Smallest Number With All Set Bits
+// URL: https://leetcode.com/problems/smallest-number-with-all-set-bits/
+// ============================================================================
+
+/*
+	goos: linux
+	goarch: amd64
+	pkg: GoLeetCode/solutions/3370---Smallest-Number-With-All-Set-Bits
+	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
+	Benchmark_isBalanced
+	Benchmark_isBalanced-24    	53495989	        26.21 ns/op	       0 B/op	       0 allocs/op
+	PASS
+*/
+
+func smallestNumber(n int) int {
+	i := 0
+	for {
+		v := int(math.Pow(float64(2), float64(i)))
+		if v > n {
+			return v - 1
+		}
+		i++
+	}
+
+	return -1
+}
