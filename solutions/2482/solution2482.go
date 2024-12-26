62d444ef86393307d745ccc7d4b7f0f6cd208fb5
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2024-12-26 19:59:49 +0100

    add 2482

---
+++ solutions/2482/solution2482.go
@@ -0,0 +1,33 @@
+package solution2482
+
+func onesMinusZeros(grid [][]int) [][]int {
+
+	onesRow := make([]int, len(grid))
+	onesCol := make([]int, len(grid[0]))
+	zerosRow := make([]int, len(grid))
+	zerosCol := make([]int, len(grid[0]))
+
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[0]); x++ {
+			n := grid[y][x]
+			if n == 0 {
+				zerosRow[y]++
+				zerosCol[x]++
+			} else {
+				onesRow[y]++
+				onesCol[x]++
+			}
+		}
+	}
+
+	output := make([][]int, 0, len(grid))
+	for y := 0; y < len(grid); y++ {
+		row := make([]int, len(grid[0]))
+		for x := 0; x < len(grid[0]); x++ {
+			row[x] = onesRow[y] + onesCol[x] - zerosRow[y] - zerosCol[x]
+		}
+		output = append(output, row)
+	}
+
+	return output
+}
