14627b85b0caf123d0b7459556b5437ef973ad66
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2024-12-29 07:48:39 +0100

    upd 3324

--- solutions/3324/solution3324.go
+++ solutions/3324/solution3324.go
@@ -11,29 +11,24 @@ package solution3324
 	pkg: GoLeetCode/solutions/3324
 	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
 	Benchmark_stringSequence
-	Benchmark_stringSequence-24    	 2006986	       564.8 ns/op	     568 B/op	      23 allocs/op
+	Benchmark_stringSequence-24    	 2483098	       420.8 ns/op	     498 B/op	      17 allocs/op
 	PASS
 */
 
 func stringSequence(target string) []string {
 
-	var sequence []string
+	sequence := []string{""}
+
+	var n int
+	var prefix string
 	for _, ch := range target {
-
-		n := int(ch-'a') + 1
-		prefix := ""
-		if len(sequence) >= 1 {
-			l := len(sequence[len(sequence)-1])
-			prefix += sequence[len(sequence)-1][:l]
-		}
-
+		n = int(ch-'a') + 1
+		prefix = sequence[len(sequence)-1]
 		for i := 0; i < n; i++ {
-			s := prefix + ""
-			s += string(rune(i + 'a'))
-			sequence = append(sequence, s)
+			sequence = append(sequence, prefix+string(rune(i+'a')))
 
 		}
 	}
 
-	return sequence
+	return sequence[1:]
 }
