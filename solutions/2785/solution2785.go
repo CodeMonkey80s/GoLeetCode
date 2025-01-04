3345a2f413043fe29c60336f78accbbbb600e36e
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2025-01-04 02:12:04 +0100

    upd 2785

--- solutions/2785/solution2785.go
+++ solutions/2785/solution2785.go
@@ -5,12 +5,52 @@ package solution2785
 // URL: https://leetcode.com/problems/sort-vowels-in-a-string/
 // ============================================================================
 
+/*
+	goos: linux
+	goarch: amd64
+	pkg: GoLeetCode/solutions/2785
+	cpu: 13th Gen Intel(R) Core(TM) i7-13700K
+	Benchmark_sortVowelsV2
+	Benchmark_sortVowelsV2-24    	 9544694	       113.7 ns/op	      16 B/op	       2 allocs/op
+	Benchmark_sortVowelsV1
+	Benchmark_sortVowelsV1-24    	 7979403	       129.8 ns/op	      32 B/op	       3 allocs/op
+	PASS
+*/
+
 import (
 	"slices"
 	"strings"
 )
 
-func sortVowels(s string) string {
+func sortVowelsV2(s string) string {
+
+	const (
+		chars = "AEIOUaeiou"
+	)
+
+	var vowels []byte
+	for _, ru := range s {
+		if strings.ContainsRune(chars, ru) {
+			vowels = append(vowels, byte(ru))
+		}
+	}
+
+	slices.Sort(vowels)
+
+	b := []byte(s)
+
+	var j int
+	for i, ru := range s {
+		if strings.ContainsRune(chars, ru) {
+			b[i] = vowels[j]
+			j++
+		}
+	}
+
+	return string(b)
+}
+
+func sortVowelsV1(s string) string {
 
 	const (
 		chars = "AEIOUaeiou"
--- solutions/2785/solution2785_test.go
+++ solutions/2785/solution2785_test.go
@@ -24,10 +24,22 @@ func Test_sortVowels(t *testing.T) {
 	for _, tc := range testCases {
 		label = fmt.Sprintf("Case: Input: %v, Output: %v\n", tc.Input, tc.Output)
 		t.Run(label, func(t *testing.T) {
-			output := sortVowels(tc.Input)
+			output := sortVowelsV2(tc.Input)
 			if output != tc.Output {
 				t.Errorf("Expected output to be %q but we got %q", tc.Output, output)
 			}
 		})
+	}
+}
+
+func Benchmark_sortVowelsV2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = sortVowelsV2(testCases[0].Input)
+	}
+}
+
+func Benchmark_sortVowelsV1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = sortVowelsV1(testCases[0].Input)
 	}
 }
